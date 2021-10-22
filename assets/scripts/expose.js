7bad2a41f0db8256fb681b7450569b6926814006
Edgar <eddiaz@ucsd.edu>
2021-10-22 15:40:03 -0700

    stuff

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,8 +4,10 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const selectElement = document.getElementById('horn-select');
+  var confetti;
+  var sound;
+  var audiosrc;
   selectElement.addEventListener('change', (event) =>{
-    var confetti;
     let img = document.querySelector("img");
     let image = "assets/images/"+event.target.value+".svg";
     if(event.target.value == "party-horn"){
@@ -15,22 +17,41 @@ function init() {
       confetti = false;
     }
     img.src = image;
-    var sound = document.getElementsByClassName("hidden");
-    var audiosrc = "assets/audio/"+event.target.value+".mp3";
-    sound.src = audiosrc;
+    sound = document.getElementsByClassName("hidden");
+    audiosrc = "assets/audio/"+event.target.value+".mp3";
+    sound = new Audio(audiosrc);
   });
-  const buttonElement = document.querySelector("button");
-  buttonElement.addEventListener('click', () =>{
-    sound.play();
-    if(confetti){
-      const jsConfetti = new JSConfetti()
-      jsConfetti.addConfetti({
-        confettiColors: [
-          '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
-        ],
-      })
+  const slideElement = document.getElementById('volume-controls');
+  slideElement.addEventListener('change', (event) =>{
+    let img2 = slideElement.querySelector("img");
+    if(event.target.value == 0){
+      img2.src = "assets/icons/volume-level-0.svg";
+      sound.volume = 0;
+    }
+    else if(event.target.value > 0 && event.target.value < 33){
+      img2.src = "assets/icons/volume-level-1.svg";
+      sound.volume = 0.33;
+    }
+    else if(event.target.value >= 33 && event.target.value < 67){
+      img2.src = "assets/icons/volume-level-2.svg";
+      sound.volume = 0.66;
+    }
+    else if(event.target.value >= 67){
+      img2.src = "assets/icons/volume-level-3.svg";
+      sound.volume = 1;
     }
   })
-  const slideElement = document.getElementById('volume-controls');
-  slideElement.addEventListener('change',
+  const buttonElement = document.querySelector("button");
+  buttonElement.addEventListener('click', function(){
+    sound.play();
+    console.log(sound);
+    if(confetti){
+      const jsConfetti = new jsConfetti();
+      jsConfetti.addConfetti();
+ /*       confettiColors: [
+          '#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd', '#f9bec7',
+        ],*/
+     // )
+    }
+  })
 }
