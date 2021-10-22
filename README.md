76f7975732db664e57ebb3eb0771be867b5e1713
Edgar <eddiaz@ucsd.edu>
2021-10-21 20:44:05 -0700

    Names added and image appears

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lab 5 - Starter
+Name(s): Edgar Diaz and Adan Estrada
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,13 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById('horn-select');
+  selectElement.addEventListener('change', (event) =>{
+    const img = document.querySelector("img");
+    let image = "assets/images/"+event.target.value+".svg";
+    img.src = image;
+    const sound = document.getElementsByClassName("hidden");
+    let audiosrc = "assets/audio/"+event.target.value+".mp3";
+    sound.src = audiosrc;
+  });
 }
